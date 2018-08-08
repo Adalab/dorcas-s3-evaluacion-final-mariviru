1d36cf5ce9e4cafecb8d01c22427c0fe90190c25
Mariví <marivirh82@gmail.com>
2018-08-08 16:05:36 +0200

    add fetch function and render list

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,42 @@ import React, { Component } from 'react';
 import List from './List';
 import './App.css';
 
+const URL = 'http://hp-api.herokuapp.com/api/characters';
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      characterList: [],
+    }
+
+    this.fetchNewList = this.fetchNewList.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchNewList();
+  }
+
+  fetchNewList() {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('data', data)
+        this.setState({
+          characterList: data,
+        },
+        )
+        console.log('characterList', this.state)
+      })
+  }
+
   render() {
+    console.log('app state', this.state.characterList)
     return (
-      <div className="App">
-      
-      </div>
+      <List
+        characterList = {this.state.characterList}
+      />
     );
   }
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
 class List extends Component {
-    render() { 
-        return ( 
-            <div></div>
-         );
+
+    render() {
+        console.log('list props', this.props)
+        const characterList = this.props.characterList
+        console.log('character list in list', characterList)
+        return (
+            <div>
+                <ul>
+                    {characterList.map(function (character, index) {
+                        return (
+                            <li key={index} >{character.name}
+                                <img src={character.image} alt={character.name}/>
+                                <span>{character.house}</span>
+                            </li>
+                        )
+                    }
+                    )}
+                </ul>
+            </div>
+        );
     }
 }
- 
+
 export default List;
