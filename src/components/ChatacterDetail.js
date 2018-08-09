3eb897e88233bd10e3ce4db00f6c083cb6cd1f7f
Mariví <marivirh82@gmail.com>
2018-08-09 11:35:07 +0200

    add CharacterDetail component

---
+++ src/components/ChatacterDetail.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './CharacterDetail.css';
+
+class CharacterDetail extends Component {
+	render() { 
+    const characterList = this.props.characterList;
+    console.log('que hay en detail', characterList)
+				return (
+          <p>detalle</p>
+					// <div
+					// 	className='Character'>
+					// 	<h1 className='Character__name' >
+					// 		{character.name}
+					// 	</h1>
+					// 	<img
+					// 		src={character.image}
+					// 		alt={character.name}
+					// 		className='Character__img' />
+					// 	<span
+					// 		className='Character__house' >
+					// 		{character.house}
+					// 	</span>
+					// </div>
+				)
+			
+	}
+}
+ 
+export default CharacterDetail;
